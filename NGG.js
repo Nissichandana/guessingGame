e3e3125ecb5c16829f311cd13ef3569ba7dad874
NissiChandana <nissi.neerudu@gmail.com>
2023-11-03 19:04:39 -0400

    final-Commit-Without-Livelink

--- NGG.js
+++ NGG.js
@@ -1,22 +1,22 @@
 
-let btn = document.getElementById('btn');;
-let outPut = document.getElementById('outPutText');;
+let button = document.getElementById('btn');
+let outPut = document.getElementById('textBox');
 let attemNumber = document.getElementById('attempts');
-let number = 0;
+let rndNumber = 0;
 let noOfAttempts = 0;
 
 function initPage() {
-    btn = document.getElementById('btn');
-    outPut = document.getElementById('outPutText');
-    attemNumber = document.getElementById('attempts')
-    number = [Math.floor(Math.random() * 10) + 1];
-    console.log("Actual Number inside init function: "+number);
+    button = document.getElementById('btn');
+    outPut = document.getElementById('textBox');
+    attemNumber = document.getElementById('attempts');
+    rndNumber = [Math.floor(Math.random() * 10) + 1];
+    console.log("Actual Number inside init function: "+rndNumber);
     noOfAttempts = 3;
-    outPut.innerHTML = "Enter A Number"
+    outPut.innerHTML = " Enter Number ";
     attemNumber.innerHTML = "You have 3 attempts to guess the right number";
-    userInput.value = "";
-    userInput.focus();
-    //location.reload();
+    playerInput.value = "";
+    playerInput.focus();
+    
 }
 // let userMsg = " "
 
@@ -29,41 +29,47 @@ function initPage() {
 //     guessField.focus();
 //     console.log("randNumGenerated = " + randNumGenerated);
 // }
-
-btn.addEventListener('click', function guessNum() {
-    let input = document.getElementById('userInput').value;
+let input = document.getElementById("playerInput");
+input.addEventListener("keypress",function(event){
+    if(event.key === "Enter"){
+        event.preventDefault();
+        document.getElementById("btn").click();
+    }
+})
+button.addEventListener('click', function numGuess() {
+    let input = document.getElementById('playerInput').value;
     console.log("number of attempts " + noOfAttempts);
     let guessed = false;
 
     if (noOfAttempts > 0) {
-        console.log("number: " + number);
-        console.log("Entered Numebr: "+input+"  Actual Num: "+number);
+        console.log("number: " + rndNumber);
+        console.log("Entered Numebr: "+input+"  Actual Num: "+rndNumber);
+
         if (isNaN(input) || input < 1 || input > 10) {
             console.log("2....");
             outPut.innerHTML = "Please Enter a Valid Number between 1 and 10";
             noOfAttempts--;
             attemNumber.innerHTML = "You have " + noOfAttempts + " attempts to guess the right number";
 
-        } else if (input == number) {
+        } else if (input == rndNumber) {
             console.log("3....");
-            outPut.innerHTML = `You guessed it right, the number was ${number} !`;
+            outPut.innerHTML = `You guessed it right, the number was ${rndNumber} !`;
             noOfAttempts--;
-            attemNumber.innerHTML = "YEPEEE....the number was " + number + " ! ";
+            attemNumber.innerHTML = "YEPEEE.... ";
             setTimeout(()=> {
                 initPage();
-                //document.getElementById("result").innerHTML = "Hello, I am here";
              }
              ,5000);
             guessed = true;
             
 
-        } else if (parseInt(input) < parseInt(number)) {
+        } else if (parseInt(input) < parseInt(rndNumber)) {
             console.log("4....");
             outPut.innerHTML = `you guessed too low!`;
             noOfAttempts--;
             attemNumber.innerHTML = "You have " + noOfAttempts + " attempts to guess the right number";
 
-        } else if (parseInt(input) > parseInt(number)) {
+        } else if (parseInt(input) > parseInt(rndNumber)) {
             console.log("5....");
             outPut.innerHTML = 'you guessed too high';
             noOfAttempts--;
@@ -71,10 +77,9 @@ btn.addEventListener('click', function guessNum() {
         }
     }
     if (noOfAttempts == 0 && !guessed) {
-        attemNumber.innerHTML = "You are out of attempts. Original number was " + number;
+        attemNumber.innerHTML = "You are out of attempts. Original number was " + rndNumber;
         setTimeout(()=> {
             initPage();
-            //document.getElementById("result").innerHTML = "Hello, I am here";
          }
          ,5000);
     }
--- README.md
+++ README.md
@@ -1,1 +1,33 @@
-# guessingGame
+# Guess The Number Game
+
+# instructions
+
+User has three attempts to guess the number between 1-10.  Player enters a number and click the submit button, then the computer checks the number with the random generated number using and matches it and gives the output if the number is too low or too high. If the input number mathches with the random generated number then the player wins the game. If the player couldn't guess the number in three atempts then a message displays saying you are out of your attempts and gives the number. If the user enters any number out of range an error message will be displayed and the player loses an attempt. I have a Timeout() method to refresh the page after 5 seconds instead of reset button. I also added Trigger a Button Click on Enter to have an access to both sumbit and Enter button on the keyboard. The player has an option of 
+
+
+# wireframe
+![Alt text](<Mod1-Final-Project (1).jpg>)
+
+# flowchart
+(flowchart-pic.jpeg)
+# image
+![Alt text](bg-nu-light-1.jpg)
+
+# live site link
+
+# Resource
+
+1. miro.com
+2. https://www.w3schools.com/howto/howto_js_trigger_button_enter.asp
+3. https://www.w3schools.com/jsref/jsref_parseint.asp
+4. https://www.w3schools.com/jsref/prop_html_innerhtml.asp
+5. https://www.w3schools.com/cssref/pr_background-image.php
+6. https://www.w3schools.com/jsref/event_onload.asp
+7. https://pixabay.com/illustrations/pay-digit-number-abundance-count-2446661/
+8. https://www.youtube.com/watch?v=2cQUkYU8AmI
+9. https://www.tutorialspoint.com/How-to-delay-a-JavaScript-function-call-using-JavaScript
+10. my instructor, Christina
+11. my friends
+12. my husband
+
+
